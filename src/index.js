d708011f95ecc749c1c2a7205388e2b7540e42d1
praneeta <praneetamhatre@gmail.com>
2017-10-26 15:55:48 -0700

    passing callbacks

--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search'
+import YTSearch from 'youtube-api-search';
+import FullVideo from './components/full_video';
+import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBVmrrVrGinCVQuIyv9kJbfSj7PC7jsNes';
 
-YTSearch({key: API_KEY, term: 'double double toil and trouble'}, (data) => {
-  console.log(data);
-})
+class App extends Component{
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'double double toil and trouble'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <FullVideo video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
